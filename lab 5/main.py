2d32d3909858e558145e03f451bad01eefde8dc9
Adiya <zhakyp.adiya@gmail.com>
2026-04-23 08:31:35 +0500

    change lab 5

--- lab 5/main.py
+++ lab 5/main.py
@@ -39,6 +39,8 @@ class Product:
         self.category = category
     def __str__(self):
         return f"Product(id={self.id}, name='{self.name}', price={self.price}, category='{self.category}')"
+    def __repr__(self):
+        return self.__str__()
     def __eq__(self, other):
         return isinstance(other, Product) and self.id == other.id
     def __hash__(self):
